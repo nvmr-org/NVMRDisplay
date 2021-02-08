82bf31a0a55f059a1acb8fd642c88d15ebf7fa77
Robert Middleton <robert.middleton@rm5248.com>
2021-02-07 20:25:39 -0500

    removed bad gstreamer plugin

--- prototype/rpi-video.sh
+++ prototype/rpi-video.sh
@@ -3,7 +3,6 @@
 gst-launch-1.0 udpsrc multicast-group=224.1.1.3 auto-multicast=true port=8230 \
   ! 'application/x-rtp,width=1280,height=720' \
   ! rtph264depay \
-  ! h264parse \
   ! avdec_h264 \
   ! autovideosink sync=false
 
