9dbcc4d6fdb4e0955b937583598bac262ec05e76
Robert Middleton <robert.middleton@rm5248.com>
2021-08-15 15:42:33 -0400

    change width and height to variables

--- prototype/rpi-video.sh
+++ prototype/rpi-video.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+width=1280
+height=720
+
+#  ! jpegenc snapshot=TRUE ! filesink location=/tmp/foo.jpg
+
 gst-launch-1.0 udpsrc multicast-group=224.1.1.3 auto-multicast=true port=8230 \
-  ! 'application/x-rtp,width=1280,height=720' \
+  ! "application/x-rtp,width=$width,height=$height" \
   ! rtph264depay \
   ! avdec_h264 \
   ! autovideosink sync=false
