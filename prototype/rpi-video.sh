b5432617d1e4172f932608ec13b7c4ad9d331a9b
Robert Middleton <robert.middleton@rm5248.com>
2022-09-01 22:07:51 -0400

    messing with gstreamer

--- prototype/rpi-video.sh
+++ prototype/rpi-video.sh
@@ -5,9 +5,19 @@ height=720
 
 #  ! jpegenc snapshot=TRUE ! filesink location=/tmp/foo.jpg
 
-gst-launch-1.0 udpsrc multicast-group=224.1.1.3 auto-multicast=true port=8230 \
+gst-launch-1.0 udpsrc port=8230 \
   ! "application/x-rtp,width=$width,height=$height" \
   ! rtph264depay \
   ! avdec_h264 \
   ! autovideosink sync=false
+
+exit 0
+
+gst-launch-1.0 udpsrc port=8230 \
+  ! "application/x-rtp,width=$width,height=$height,framerate=1/24,encoding-name=JPEG" \
+  ! rtpjpegdepay \
+  ! jpegdec \
+  ! queue \
+  ! fpsdisplaysink 
+#  ! autovideosink
 
